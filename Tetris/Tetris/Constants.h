cb8fd27244c4157a63d826d357b311d9844ba097
Marcin Kuchta <marcinkuchta98@gmail.com>
2019-01-18 19:04:09 +0100

    Final size of the window and blocks

--- Tetris/Tetris/Constants.h
+++ Tetris/Tetris/Constants.h
@@ -1,9 +1,11 @@
 #ifndef _CONSTANTS_H_
 #define _CONSTANTS_H_
 
-#define WIDTH 400
-#define HEIGHT 800
+#define WIDTH 350
+#define HEIGHT 700
+#define WIDTH_IN_BLOCKS 14
+#define HEIGHT_IN_BLOCKS 28
 #define BLOCK_SIZE 25
 
-#endif // !_CONSTANTS_H
+#endif 
 
