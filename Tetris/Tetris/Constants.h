0fed065d237a6468fd277ce4102221481279c373
Marcin Kuchta <marcinkuchta98@gmail.com>
2019-01-19 20:47:55 +0100

    Syntax

--- Tetris/Tetris/Constants.h
+++ Tetris/Tetris/Constants.h
@@ -1,11 +1,15 @@
 #ifndef _CONSTANTS_H_
 #define _CONSTANTS_H_
 
+#define BLOCK_SIZE 25
+
 #define WIDTH 350
 #define HEIGHT 700
-#define WIDTH_IN_BLOCKS 14
-#define HEIGHT_IN_BLOCKS 28
-#define BLOCK_SIZE 25
+
+#define WIDTH_IN_BLOCKS WIDTH/BLOCK_SIZE
+#define HEIGHT_IN_BLOCKS HEIGHT/BLOCK_SIZE
+
+#define NUMBER_OF_CHOICES 2
 
 #endif 
 
